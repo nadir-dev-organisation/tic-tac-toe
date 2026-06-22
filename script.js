f18a5c5c82c43eb828afc02ba9bfe3f3d18d10a2
TableB4sse <randomguy8242@gmail.com>
2026-06-22 21:49:52 +0200

    Add avatar transition when clicking bottom button

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
 const crossMarkBtn = document.querySelector(".cross-mark");
 const noughtMarkBtn = document.querySelector(".nought-mark");
+const playerNameBtn = document.querySelectorAll(".player-name");
+const botDifficultyBtn = document.querySelectorAll(".bot-difficulty");
 
 function toggleMark(markButton, secondMarkButton){
   markButton.textContent = markButton.textContent.trim() === "X" ? "O" : "X";
   secondMarkButton.textContent = secondMarkButton.textContent.trim() === "X" ? "O" : "X";
+}
+
+function toggleSelected(buttonSelected, secondButton) {
+  buttonSelected.classList.toggle("is-selected");
+
+  if (secondButton.classList.contains("is-selected")) {
+    secondButton.classList.toggle("is-selected");
+  }
+}
+
+function toggleAvatar(cardButton) {
+  const card = cardButton.closest(".card-player");
+  const avatar = card.querySelector(".avatar");
+  avatar.classList.add("is-changing");
+
+  if (cardButton.classList.contains("player-name")) {
+    setTimeout(() => {
+      avatar.addEventListener("load", () => {
+        avatar.classList.remove("is-changing");
+        avatar.classList.add("is-entering");
+
+        requestAnimationFrame(() => {
+          avatar.classList.remove("is-entering");
+        });
+
+      }, {once: true});
+      avatar.src = "img/player.gif";
+    }, 320);
+  } if(cardButton.classList.contains("bot-difficulty")) {
+    setTimeout(() => {
+      avatar.addEventListener("load", () => {
+        avatar.classList.remove("is-changing");
+        avatar.classList.add("is-entering");
+
+        requestAnimationFrame(() => {
+          avatar.classList.remove("is-entering");
+        });
+
+      }, {once: true});
+      avatar.src = "img/bot.gif";
+    }, 320);
+  }
 }
 
 function setupEventListener(){
@@ -13,7 +57,24 @@ function setupEventListener(){
   noughtMarkBtn.addEventListener("click", (event) => {
     toggleMark(event.currentTarget, crossMarkBtn);
   });
-
+  playerNameBtn.forEach((playerButton) => {
+    playerButton.addEventListener("click", (event) => {
+      const wasSelected = event.currentTarget.classList.contains("is-selected");
+      toggleSelected(event.currentTarget, event.currentTarget.nextElementSibling);
+      if (!wasSelected) {
+        toggleAvatar(event.currentTarget);
+      }
+    });
+  });
+  botDifficultyBtn.forEach((botButton) => {
+    botButton.addEventListener("click", (event) => {
+      const wasSelected = event.currentTarget.classList.contains("is-selected");
+      toggleSelected(event.currentTarget, event.currentTarget.previousElementSibling);
+      if (!wasSelected) {
+        toggleAvatar(event.currentTarget);
+      }
+    });
+  });
 }
 
 setupEventListener();
